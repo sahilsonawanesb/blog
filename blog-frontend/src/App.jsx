9ebf604747f05cf0615ed683243ab43169f2f571
Sahil Sonawane <sonawanesahil360@gmail.com>
2025-01-14 09:53:25 +0530

    React Router and Pages created

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -1,8 +1,25 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from './pages/Projects';
+import Dashboard from "./pages/Dashboard";
+
 const App = () => {
   return (
-    <h1 className="text-3xl text-red-500">
-      MERN Blog Website..
-    </h1>
+    <BrowserRouter>
+    
+    <Routes>
+      <Route path="/" element ={<Home />} />
+      <Route path="/about" element={<About/>}/>
+      <Route path="/signIn" element={<SignIn/>}/>
+      <Route path="/signUp" element={<SignUp/>}/>
+      <Route path="/projects" element={<Projects/>}/>
+      <Route path="/dashboard" element={<Dashboard />}/>
+    </Routes>
+
+    </BrowserRouter>
   )
 }
 
