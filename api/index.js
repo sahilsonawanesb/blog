aa3cca78dae33eae6dc491adf47d231e610858fb
Sahil Sonawane <sonawanesahil360@gmail.com>
2025-01-15 14:25:40 +0530

    Database connection done

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // create app
 const app = express();
+// connect to the database now
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log('MongoDb is Connected');
+    }).catch((error) => {
+        console.log(error);
+    });
 
 // listen port
 app.listen(3000, () => {
