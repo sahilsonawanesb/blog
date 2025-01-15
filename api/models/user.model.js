15cd1a46467d29736b0cebcfde07412c19c5b729
Sahil Sonawane <sonawanesahil360@gmail.com>
2025-01-15 14:50:41 +0530

    user model created

---
+++ api/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+
+// create schema for the users.
+const userSchema = new mongoose.Schema({
+    userName : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    password : {
+        type : String,
+        required : true,
+    },
+}, {timestamps: true}
+
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
