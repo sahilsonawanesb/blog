9d9e2a39a922deac94b8aac0fe8da018075cd00f
Sahil Sonawane <sonawanesahil360@gmail.com>
2025-01-15 11:36:44 +0530

    Created Header Section

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Header  from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
@@ -9,7 +10,7 @@ import Dashboard from "./pages/Dashboard";
 const App = () => {
   return (
     <BrowserRouter>
-    
+    <Header />
     <Routes>
       <Route path="/" element ={<Home />} />
       <Route path="/about" element={<About/>}/>
