18e3d2abf10089bdda671673515ff34b79b67e76
Sahil Sonawane <sonawanesahil360@gmail.com>
2025-01-16 22:01:56 +0530

    add redux persist

--- blog-frontend/src/redux/store.js
+++ blog-frontend/src/redux/store.js
@@ -1,8 +1,27 @@
-import {configureStore} from "@reduxjs/toolkit";
+/* eslint-disable no-undef */
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import userReducer from './user/userSlice';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistStore from "redux-persist/es/persistStore";
+
+// combining all reducers
+const rootReducer = combineReducers({
+    user : userReducer,
+})
+
+const persistConfig = {
+    key : 'root',
+    storage,
+    version : 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer :  {
-        user : userReducer,
-    },
+    reducer :  persistedReducer,
+    middleware : (getDefaultMiddlewre) => 
+        getDefaultMiddlewre({ serializableCheck : false}),
 });
+
+export const persistor = persistStore(store);
