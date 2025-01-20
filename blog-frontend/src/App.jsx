45bd1223ec229f32d10c04bf7b216d3d9710ef35
Sahil Sonawane <sonawanesahil360@gmail.com>
2025-01-20 17:48:39 +0530

    Complete post page functionality

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
       <Route path="/signIn" element={<SignIn/>}/>
       <Route path="/signUp" element={<SignUp/>}/>
       <Route path="/projects" element={<Projects/>}/>
+      <Route path="/post/:postSlug" element={<PostPage />}/>
       <Route element={<PrivateRoute />}>
       <Route path="/dashboard" element={<Dashboard />}/>
       </Route> 
