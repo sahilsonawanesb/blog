7dc854f768ff6e831b2381249d15f5abf96d2c95
Sahil Sonawane <sonawanesahil360@gmail.com>
2025-01-22 11:35:19 +0530

    complete about and project section

--- blog-frontend/src/pages/About.jsx
+++ blog-frontend/src/pages/About.jsx
@@ -1,10 +1,25 @@
 const About = () => {
   return (
-    <div>
-        <h1>
-            About Page
-        </h1>
-      
+    <div className="min-h-screen flex items-center justify-center">
+        <div className="max-w-2xl mx-auto p-3 text-center">
+          <div className="">
+            <h1 className="text-3xl font font-semibold text-center py-7">About Sahil Blog&apos;s  </h1>
+            <div className="text-md text-gray-500 flex flex-col gap-6">
+              <p>
+              Hi, I’m Sahil, a passionate software developer with expertise in full-stack development, specializing in modern technologies like the MERN stack, React, and TypeScript. 
+              I love building innovative, scalable applications that solve real-world problems and make a positive impact.
+              </p>
+              <p>
+              When I’m not coding, I’m exploring advancements in AI, working on projects like personalized learning platforms and sustainability-focused applications. 
+              I’m driven by a problem-solving mindset and a commitment to creating efficient, user-friendly solutions.
+              </p>
+              <p>
+              Outside of work, I enjoy learning, mentoring, and collaborating on new ideas. Through this blog, 
+              I aim to share my journey, projects, and insights to inspire and connect with like-minded individuals.
+              </p>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
--- blog-frontend/src/pages/Projects.jsx
+++ blog-frontend/src/pages/Projects.jsx
@@ -1,9 +1,13 @@
+import CallToAction from "../components/CallToAction"
+
 const Projects = () => {
   return (
-    <div>
-      <h1>
-        Projects Page
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">
+        Projects
       </h1>
+      <p className="text-md text-gray-500 ">Build fun and engaging projects while learning HTML, CSS, Javascript, React js and MERN</p>
+      <CallToAction />
     </div>
   )
 }
