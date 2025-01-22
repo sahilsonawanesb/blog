a01056bc22b8948e78dd6ad709e088bd3caf2631
Sahil Sonawane <sonawanesahil360@gmail.com>
2025-01-22 12:34:23 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,8 +6,12 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
+
+
+const __dirname = path.resolve();
 
 // create app
 const app = express();
@@ -40,6 +44,13 @@ app.use('/api/post', postRoutes);
 
 // comment Router
 app.use('/api/comment', commentRoutes);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 // creating the middleware for handling rh error.
